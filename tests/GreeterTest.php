e36a5ccec1653d7ec87f76c98900f853dda3a6be
ChaimaeHB <chaimaehallab99@gmail.com>
2025-07-02 12:42:01 +0200

    Ajout du test Greeter

---
+++ tests/GreeterTest.php
@@ -0,0 +1,15 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use Chaimae\ProjetAtelier1\Greeter;
+
+class GreeterTest extends TestCase
+{
+    public function testGreetReturnsExpectedMessage(): void
+    {
+        $greeter = new Greeter();
+        $this->assertEquals('Hello from src!', $greeter->greet());
+    }
+}
+?>
+
